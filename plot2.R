095334e30e4562dee7e0ef32d9b84e39fc790fcb
Systemocracy <systemocracy@gmail.com>
2015-01-12 00:57:52 +0100

    Project R and png files

---
+++ plot2.R
@@ -0,0 +1,17 @@
+# Load data
+epconsumption <- read.table("./data/household_power_consumption.txt", sep=";", 
+							na.strings = "?", dec=".", header=TRUE)
+# Subset data
+epconsumption<-subset (epconsumption, epconsumption$Date=="1/2/2007" | 
+					   	epconsumption$Date=="2/2/2007")
+# Create new Date_Time variable with class DATE from string class vars Date and Time
+epconsumption$Date_Time <- paste(epconsumption$Date, epconsumption$Time)
+epconsumption$Date_Time <- strptime(epconsumption$Date_Time, "%d/%m/%Y %H:%M:%S")
+
+# Open output Device to file
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+# Plot 2
+plot(epconsumption$Date_Time, epconsumption$Global_active_power, type="l", 
+	 xlab="", ylab="Global Active Power (kilowatts)")
+# Close device
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,21 @@
+# Load data
+epconsumption <- read.table("./data/household_power_consumption.txt", sep=";", 
+							na.strings = "?", dec=".", header=TRUE)
+# Subset data
+epconsumption<-subset (epconsumption, epconsumption$Date=="1/2/2007" | 
+					   	epconsumption$Date=="2/2/2007")
+# Create new Date_Time variable with class DATE from string class vars Date and Time
+epconsumption$Date_Time <- paste(epconsumption$Date, epconsumption$Time)
+epconsumption$Date_Time <- strptime(epconsumption$Date_Time, "%d/%m/%Y %H:%M:%S")
+
+# Open output Device to file
+png(filename = "plot3.png", width = 480, height = 480, units = "px")
+# Plot 3
+plot(epconsumption$Date_Time, epconsumption$Sub_metering_1, type="l", 
+	 xlab="", ylab="Energy sub metering")
+lines(epconsumption$Date_Time, epconsumption$Sub_metering_2, col="RED")
+lines(epconsumption$Date_Time, epconsumption$Sub_metering_3, col="BLUE")
+legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
+	   lty = c(1,1,1), col=c('BLACK','RED','BLUE'))
+# Close device
+dev.off()
